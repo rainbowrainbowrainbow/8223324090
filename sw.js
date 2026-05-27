b9e763275b990e010e29b9076dd3dbe965ab6beb
Сергій <rainbowoveryoutoo@gmail.com>
2026-05-27 20:08:15 +0300

    fix: compact sidebar menu

--- sw.js
+++ sw.js
@@ -19,8 +19,8 @@
  *   <script src="js/ws.js"></script>
  */
 
-const CACHE_NAME = 'event-genix-v0.67.3';
-const API_CACHE_NAME = 'event-genix-api-v0.67.3';
+const CACHE_NAME = 'event-genix-v0.67.4';
+const API_CACHE_NAME = 'event-genix-api-v0.67.4';
 
 // App Shell — static assets to pre-cache on install
 const APP_SHELL = [
